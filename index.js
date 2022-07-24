eb70ea3b4f4339b26d7b2b6670630237dab1e607
Yoana <yoanaarias-magana@Administrators-MacBook-Pro.local>
2022-07-24 14:16:54 -0400

    july 24

--- index.js
+++ index.js
@@ -4,29 +4,47 @@ const mysql = require('mysql')
 const cors = require('cors')
 const bodyparser = require('body-parser')
 
+//starts express server
+const port = process.env.PORT || 5001
+
 app.use(express.json())
 app.use(cors())
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+//connection to mysql database in RDS
 const db = mysql.createConnection({
     user: "root",
-    host: "localhost",
-    password: "password",
+    host: "ecommproject.ccwofqolezde.us-east-1.rds.amazonaws.com",
+    password: "Catsrcool6!",
     database: "productInfo",
 });
+
+//checking to make sure database is connected
 db.connect((error)=> {
     if(error) {
         throw error
     }console.log('database connected')
 })
-app.get('/productsdb', (req, res) => {
-    db.query('SELECT * FROM products', (err, response) => {
-        if(err) {
-            throw error
-        }else {
-            res.send(response) 
-        }
-    })
-})
 
-app.listen(3001, () => {
-    console.log("Your server is running on port 3001");
+//grabbing data from my database
+app.get('/', (req, res) => {
+    db.query('SELECT * FROM products', (err, rows, fields) => {
+        if (err) throw err;
+        res.send(rows);
+    });
 });
+
+app.get('/:id', (req, res) => {
+    db.query('SELECT * FROM products WHERE id = ?', [req.params.id], (err, rows, fields) => {
+        if (err) throw err;
+        else if (rows == []) {
+            res.status(404).send('Product not found');
+        }
+        else {
+            res.send(rows);
+        }
+    });
+});
+
